fc3e453f8bb68db398182ef609a2b9eefb63ce2a
Nikiya Simpson <nikiyasimpson@gmail.com>
2019-02-19 21:48:03 -0600

    add sql statements

--- newsdb.py
+++ newsdb.py
@@ -1,10 +1,31 @@
 import psycopg2
 
-def get_articles():
+def get_top_articles():
 
   conn = psycopg2.connect("dbname=news")
   c = conn.cursor()
   c.execute("select title, count(log.id) as num_log from articles left join log on '/article/'||articles.slug = log.path group by title order by num_log desc")
-  result = c.fetchall()
-  print(result)
-  return result
+  results = c.fetchall()
+  print(results)
+
+def get_top_authors():
+
+  conn = psycopg2.connect("dbname=news")
+  c = conn.cursor()
+  c.execute("select name, count(log.id) as num_log from articles join authors on articles.author = authors.id left join log on '/article/'||articles.slug = log.path group by name order by num_log desc")
+  results = c.fetchall()
+  print(results)
+
+def get_error_percents():
+
+  conn = psycopg2.connect("dbname=news")
+  c = conn.cursor()
+  c.execute("select date(time), count(*) as errors  from log where status = '404 NOT FOUND' group by date(time) order by errors desc ")
+  results = c.fetchall()
+  print(results)
+
+
+
+get_top_articles()
+get_top_authors()
+get_error_percents()
