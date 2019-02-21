010b8a5e6a0a6269e0b32a2aabfb09f19f692396
Nikiya Simpson <nikiyasimpson@gmail.com>
2019-02-21 10:53:54 -0600

    updated SQL strings to triple quoted string literals

--- log_analysis.py
+++ log_analysis.py
@@ -19,9 +19,9 @@ def run_query(query):
 def get_top_articles():
     """ Print the top performing article views """
 
-    query = "select title, count(log.id) as views from articles " \
-            "left join log on '/article/'||articles.slug " \
-            "= log.path group by title order by views desc limit 3"
+    query = """select title, count(log.id) as views from articles
+             left join log on '/article/'||articles.slug
+             = log.path group by title order by views desc limit 3 """
 
     results = run_query(query)
     print_table("Top Articles", results, "views")
@@ -30,10 +30,10 @@ def get_top_articles():
 def get_top_authors():
     """ Print the top authors with the most views """
 
-    query = "select name, count(log.id) as views from articles " \
-            "join authors on articles.author = authors.id " \
-            "left join log on '/article/'||articles.slug = "\
-            "log.path group by name order by views desc"
+    query = """select name, count(log.id) as views from articles
+             join authors on articles.author = authors.id
+             left join log on '/article/'||articles.slug =
+             log.path group by name order by views desc"""
 
     results = run_query(query)
     print_table("Top Authors", results, "views")
@@ -43,11 +43,11 @@ def get_error_percents():
     """ Print dates with more than 1% of request errors
     and the percent error rate """
 
-    query = "select TO_CHAR(date,'fmMonth DD, YYYY'), " \
-            "round((error_count::decimal/total_requests)*100,2)::text" \
-            " ||'%'  as error_percent  from LOG_STATUS_SUMMARY_VIEW where  "\
-            "(error_count::decimal/total_requests) > 0.01 " \
-            "order by error_percent desc"
+    query = """select TO_CHAR(date,'fmMonth DD, YYYY'),
+             round((error_count::decimal/total_requests)*100,2)::text
+             ||'%'  as error_percent  from LOG_STATUS_SUMMARY_VIEW where
+             (error_count::decimal/total_requests) > 0.01
+             order by error_percent desc"""
 
     results = run_query(query)
     print_table("Days with More than 1% Errors", results, "errors")
