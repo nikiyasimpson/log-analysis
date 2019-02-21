b67f6eae81d20fd8bcca8513fc08dd1dd7174e63
Nikiya Simpson <nikiyasimpson@gmail.com>
2019-02-20 20:07:21 -0600

    Add main function

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Log Analysis
 
 Using data from Udacity Full Stack program, will analysis SQL data set of news articles and log files from HTTP requests to populate a report
+
+##Views
+
+create view LOG_STATUS_SUMMARY_VIEW as select date(time), SUM(case when  status = '404 NOT FOUND' then 1 ELSE 0  END) as ERROR_COUNT, SUM(case when status = '200 OK' then 1 ELSE 0 END) as OK_COUNT,  COUNT(1) as TOTAL_REQUESTS  from log  group by date(time);
--- newsdb.py
+++ newsdb.py
@@ -1,31 +1,58 @@
 import psycopg2
 
-def get_top_articles():
+def run_query(query):
+    conn = psycopg2.connect("dbname=news")
+    c = conn.cursor()
+    c.execute(query)
+    return c.fetchall()
 
-  conn = psycopg2.connect("dbname=news")
-  c = conn.cursor()
-  c.execute("select title, count(log.id) as num_log from articles left join log on '/article/'||articles.slug = log.path group by title order by num_log desc")
-  results = c.fetchall()
-  print(results)
+
+def get_top_articles():
+   
+    query = "select title, count(log.id) as num_log from articles " \
+            "left join log on '/article/'||articles.slug " \
+            "= log.path group by title order by num_log desc"
+    
+    results = run_query(query)
+    print_table("Top Articles", results, "views")
+
 
 def get_top_authors():
 
-  conn = psycopg2.connect("dbname=news")
-  c = conn.cursor()
-  c.execute("select name, count(log.id) as num_log from articles join authors on articles.author = authors.id left join log on '/article/'||articles.slug = log.path group by name order by num_log desc")
-  results = c.fetchall()
-  print(results)
+    query = "select name, count(log.id) as num_log from articles " \
+            "join authors on articles.author = authors.id " \
+            "left join log on '/article/'||articles.slug = "\
+            "log.path group by name order by num_log desc"
+
+    results = run_query(query)
+    print_table("Top Authors", results, "views")
+
 
 def get_error_percents():
 
-  conn = psycopg2.connect("dbname=news")
-  c = conn.cursor()
-  c.execute("select date(time), count(*) as errors  from log where status = '404 NOT FOUND' group by date(time) order by errors desc ")
-  results = c.fetchall()
-  print(results)
+    query = "select TO_CHAR(date,'Month DD, YYYY'), " \
+            "round((error_count::decimal/total_requests)*100,2)::text" \
+            " ||'%'  as error_percent  from LOG_STATUS_SUMMARY_VIEW where  "\
+            "(error_count::decimal/total_requests) > 0.01 " \
+            "order by error_percent desc"
+
+    results = run_query(query)
+    print_table("Days with More than 1% Errors", results, "errors")
 
 
+def print_table(title, table, descriptor):
 
-get_top_articles()
-get_top_authors()
-get_error_percents()
+    print(title + "\n")
+    for row in table:
+        description, value = row
+        print('\t{} - {} {}'.format(description, value, descriptor))
+    print("\n")
+
+def main():
+    print("Log Analysis Summary" + "\n")
+    get_top_articles()
+    get_top_authors()
+    get_error_percents()
+  
+main()
+
